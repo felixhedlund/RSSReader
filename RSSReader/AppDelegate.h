e647fae61560ac8c73ff35e6fcd264567f801106
Felix Hedlund <felixhedlund@me.com>
2015-09-17 14:50:40 +0200

    Initial Commit

---
+++ RSSReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSSReader
+//
+//  Created by Felix Hedlund on 17/09/2015.
+//  Copyright (c) 2015 FelixHedlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSSReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSSReader
+//
+//  Created by Felix Hedlund on 17/09/2015.
+//  Copyright (c) 2015 FelixHedlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
